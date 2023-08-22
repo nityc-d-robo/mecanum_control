8b5cf39d17f45e7bbc17c8fb6bb76c6ea03ea2d4
sarukiti <sarukiti1891@gmail.com>
2023-08-23 03:15:09 +0900

    第二弾　殺す

--- src/mecanum_control.cpp
+++ src/mecanum_control.cpp
@@ -5,14 +5,24 @@
 #include <cmath>
 #include <geometry_msgs/msg/twist.hpp>
 #include <cstdio>
-#include <motor_lib/motor_lib.hpp>
 #include <rclcpp/qos.hpp>
 #include <rclcpp/rclcpp.hpp>
+#include "drobo_interfaces/msg/md_lib_msg.hpp"
 
 constexpr double MECANUNM_DIA = 0.152;
 constexpr double ROBOT_CENTER_TO_WHEEL_DISTANCE = 0.37; // ロボットの重心からメカナムホイールまでの距離
 
-void moveChassis(double _xrpm, double _yrpm, double _yaw) {
+void MecanunmControl::_sendPwm(uint8_t _address, uint8_t _semi_id, bool _phase, uint16_t _power){
+    auto msg = std::make_shared<drobo_interfaces::msg::MdLibMsg>();
+    msg->address = _address;
+    msg->semi_id = _semi_id;
+    msg->mode = 2; //MotorLibのPWMモードに倣いました
+    msg->phase = _phase;
+    msg->power = _power;
+    _publisher->publish(*msg);
+}
+
+void MecanunmControl::_moveChassis(double _xrpm, double _yrpm, double _yaw) {
     /// TODO:制御式落とし込んでsendSpeed()する
     double speed_abs = sqrt(pow(_xrpm, 2) + pow(_yrpm, 2));
     double radwimps = atan2(_yrpm, _xrpm);
@@ -24,10 +34,10 @@ void moveChassis(double _xrpm, double _yrpm, double _yaw) {
 
     printf("wheel1: %lf, wheel2: %lf, wheel3: %lf, wheel4: %lf\n", wheel1_rpm, wheel2_rpm, wheel3_rpm, wheel4_rpm);
 
-    MotorLib::md.sendPwm(0x00, NULL, wheel1_rpm >= 0 ? true : false, abs(wheel1_rpm), 5000);
-    MotorLib::md.sendPwm(0x01, NULL, wheel2_rpm >= 0 ? true : false, abs(wheel2_rpm), 5000);
-    MotorLib::md.sendPwm(0x02, NULL, wheel3_rpm >= 0 ? false : true, abs(wheel3_rpm), 5000);
-    MotorLib::md.sendPwm(0x03, NULL, wheel4_rpm >= 0 ? true : false, abs(wheel4_rpm), 5000);
+    _sendPwm(0x00, NULL, wheel1_rpm >= 0 ? true : false, abs(wheel1_rpm));
+    _sendPwm(0x01, NULL, wheel2_rpm >= 0 ? true : false, abs(wheel2_rpm));
+    _sendPwm(0x02, NULL, wheel3_rpm >= 0 ? false : true, abs(wheel3_rpm));
+    _sendPwm(0x03, NULL, wheel4_rpm >= 0 ? true : false, abs(wheel4_rpm));
 }
 
 void MecanunmControl::_topic_callback(
@@ -36,7 +46,7 @@ void MecanunmControl::_topic_callback(
     double yrpm = msg->linear.y / (2 * M_PI * MECANUNM_DIA) * 60;
     double yaw = msg->angular.z;
     RCLCPP_INFO(this->get_logger(), "xrpm: %lf, yrpm: %lf, yaw: %lf\n", xrpm, yrpm, yaw);
-    moveChassis(xrpm, yrpm, yaw);
+    _moveChassis(xrpm, yrpm, yaw);
 }
 
 MecanunmControl::MecanunmControl(const rclcpp::NodeOptions &options)
@@ -45,6 +55,7 @@ MecanunmControl::MecanunmControl(const rclcpp::NodeOptions &options)
 MecanunmControl::MecanunmControl(const std::string &name_space,
                                  const rclcpp::NodeOptions &options)
     : Node("mecanum_control", name_space, options) {
+    _publisher = this->create_publisher<drobo_interfaces::msg::MdLibMsg>("md_driver_topic", rclcpp::QoS(10));
     _subscription = this->create_subscription<geometry_msgs::msg::Twist>(
         "cmd_vel", rclcpp::QoS(10),
         std::bind(&MecanunmControl::_topic_callback, this,
@@ -52,13 +63,6 @@ MecanunmControl::MecanunmControl(const std::string &name_space,
 }
 
 int main(int argc, char *argv[]) {
-    MotorLib::usb_config.vendor_id = 0x483;
-    MotorLib::usb_config.product_id = 0x5740;
-    MotorLib::usb_config.b_interface_number = 0;
-
-    MotorLib::usb.setUsb(&MotorLib::usb_config);
-    MotorLib::usb.openUsb();
-
     rclcpp::init(argc, argv);
     rclcpp::spin(std::make_shared<MecanunmControl>());
     rclcpp::shutdown();
